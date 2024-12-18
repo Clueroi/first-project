90187d45fea5c008675aa2c7f8fe8f8583014954
unknown <eriicsouzaromeroo@gmail.com>
2024-12-18 14:35:46 -0300

    Add: Form Validation

--- src/components/post/post.jsx
+++ src/components/post/post.jsx
@@ -9,93 +9,101 @@ import { useState } from 'react'
 
 export function Post({ author, publishedAt, content }) {
 
-    const [Comments, setComments] = useState([
-        'Post muito bacana, hein'
-    ])
+  const [Comments, setComments] = useState([
+    'Post muito bacana, hein'
+  ])
 
-    const [newCommentText, setNewCommentText] = useState('')
+  const [newCommentText, setNewCommentText] = useState('')
 
-    const datePublishedFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
-        locale:ptBR
+  const datePublishedFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR
+  })
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  })
+
+  function HandleCreateNewComment() {
+    event.preventDefault()
+
+    const newCommentText = event.target.comment.value
+
+    setComments([...Comments, newCommentText])
+    setNewCommentText('')
+  }
+
+  function handleNewCommentChange() {
+    event.target.setCustomValidity('')
+    setNewCommentText(event.target.value)
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo é obrigatório')
+  }
+
+  function deleteComment(commentToDelete) {
+    const commentWithoutDeletedOne = Comments.filter(item => {
+      return item !== commentToDelete
     })
 
-    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
-        locale:ptBR,
-        addSuffix:true
-    })
+    setComments(commentWithoutDeletedOne)
+  }
 
-    function HandleCreateNewComment(){
-        event.preventDefault()
+  const isNewCommentEmpty = newCommentText.length === 0
 
-        const newCommentText = event.target.comment.value
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar
+            src={author.avatarUrl}
+          />
+          <div className={styles.authorInfo}>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
+          </div>
+        </div>
+        <time title={datePublishedFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
+      </header>
+      <div className={styles.content}>
+        {
+          content.map(item => {
+            if (item.type === 'paragraph') {
+              return <p key={item.content}>{item.content}</p>
+            } else if (item.type === 'link') {
+              return <p key={item.content}><a href="#">{item.content}</a></p>
+            }
+          })
+        }
+      </div>
 
-        setComments([...Comments, newCommentText])
-        setNewCommentText('')
-    }
+      <form onSubmit={HandleCreateNewComment} className={styles.commentForm} action="">
+        <strong>Deixe seu feedback</strong>
+        <textarea
+          value={newCommentText}
+          onChange={handleNewCommentChange}
+          name='comment'
+          placeholder='Deixe seu comentário'
+          onInvalid={handleNewCommentInvalid}
+          required
+        />
+        <footer>
+          <button type='submit' disabled={isNewCommentEmpty}>Publicar</button>
+        </footer>
+      </form>
 
-    function handleNewCommentChange(){
-        setNewCommentText(event.target.value)
-    }
-
-    function deleteComment(commentToDelete){
-        const commentWithoutDeletedOne = Comments.filter(item => {
-            return item !== commentToDelete
-        })
-
-        setComments(commentWithoutDeletedOne)
-    }
-
-    return (
-        <article className={styles.post}>
-            <header>
-                <div className={styles.author}>
-                    <Avatar
-                        src={author.avatarUrl}
-                    />
-                    <div className={styles.authorInfo}>
-                        <strong>{author.name}</strong>
-                        <span>{author.role}</span>
-                    </div>
-                </div>
-                <time title={datePublishedFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
-            </header>
-            <div className={styles.content}>
-                {
-                    content.map(item=>{
-                        if(item.type === 'paragraph'){
-                            return <p key={item.content}>{item.content}</p>
-                        } else if(item.type === 'link'){
-                            return <p key={item.content}><a href="#">{item.content}</a></p>
-                        }
-                    })
-                }
-            </div>
-
-            <form onSubmit={HandleCreateNewComment} className={styles.commentForm} action="">
-                <strong>Deixe seu feedback</strong>
-                <textarea
-                    value={newCommentText}
-                    onChange={handleNewCommentChange}
-                    name='comment'
-                    placeholder='Deixe seu comentário'
-                    required
-                />
-                <footer>
-                    <button type='submit'>Publicar</button>
-                </footer>
-            </form>
-
-            <div className={styles.commentList}>
-                {Comments.map(comment=>{
-                    return (
-                        <Comment
-                            key={comment}
-                            content={comment}
-                            onDeleteComment={deleteComment}
-                        />
-                    )
-                })}
-            </div>
-        </article>
-    )
+      <div className={styles.commentList}>
+        {Comments.map(comment => {
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          )
+        })}
+      </div>
+    </article>
+  )
 }
